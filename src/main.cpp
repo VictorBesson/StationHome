00137cca776bcc0d74e7cac7be5946145ce30ba8
Victor <victor.besson@gmail.com>
2021-10-08 17:01:34 +0200

    Charging wifi credential

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
+#include <Preferences.h>
 
+Preferences preferences;
 
+//put your ssid and password the first time you upload the program
+String ssid = "";
+String password = "";
 
 void setup() {
   //Setup Serial
@@ -10,7 +15,18 @@ void setup() {
   Serial.println();
   Serial.println("Connected to Serial !");
 
+  //Setup Wifi Preference
+  preferences.begin("Credentials", false);
+  if(ssid.length() == 0){
+    ssid = preferences.getString("ssid", "");
+    password = preferences.getString("password", "");
+  }else{
+    preferences.putString("ssid", ssid);
+    preferences.putString("password", password);
+  }
+  preferences.end();
 
+  
 
 }
 
